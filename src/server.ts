24ac2083ecef7e81cce8f491e3f54d5300b5dfa7
Bar Agmon <111876103+m23anon@users.noreply.github.com>
2024-02-01 17:16:44 +0200

    Authentication is working

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,26 @@
 import initApp from "./app";
-const express = require('express');
+import http from 'http';
+import swaggerUI from "swagger-ui-express"
+import swaggerJsDoc from "swagger-jsdoc"
+
 initApp().then((app) => {
-  const port = process.env.PORT;
-  app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-  });
+  const options = {
+    definition: {
+      openapi: "3.0.0",
+      info: {
+        title: "Web Advanced Application development 2023 REST API",
+        version: "1.0.1",
+        description: "REST server including authentication using JWT and refresh token",
+      },
+      servers: [{ url: process.env.SERVER_URL, },],
+    },
+    apis: ["./src/routes/*.ts"],
+  };
+  const specs = swaggerJsDoc(options);
+  app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
+
+  if (process.env.NODE_ENV !== 'production') {
+    console.log('development');
+    http.createServer(app).listen(process.env.PORT);
+  }
 });
