dfc9f78e235095843b53834023ecce68781b3781
Bar Agmon <111876103+m23anon@users.noreply.github.com>
2024-02-03 15:05:14 +0200

    cross origin

--- src/app.ts
+++ src/app.ts
@@ -15,6 +15,11 @@ const initApp = (): Promise<Express> => {
       const app = express();
       app.use(bodyParser.json());
       app.use(bodyParser.urlencoded({ extended: true }));
+      app.use((req, res, next) => {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Methods", "*");
+        res.header("Access-Control-Allow-Headers", "*");
+        next();
       app.use("/auth", authRoute);
       resolve(app);
     });
